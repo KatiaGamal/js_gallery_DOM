7fcee3a8ca568bc983188e895a33a98daba4a7d4
Katia Gamal <vgkateryna1@gmail.com>
2024-06-19 14:52:02 +0300

    make build

---
+++ index.ae6e7299.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.getElementById("largeImg");document.getElementById("thumbs").addEventListener("click",function(t){var n=t.target,c=n.closest(".gallery__thumb"),d=n.closest(".list-item__link");c&&d&&(t.preventDefault(),e.src=c.src)})});
+//# sourceMappingURL=index.ae6e7299.js.map
